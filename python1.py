2c3ab0cc92a6bf1b1891539987495c4de9ee0fad
hamzahnaseem <79930387+hamzahnaseem@users.noreply.github.com>
2021-03-07 15:26:30 +0500

    Add files via upload

--- python1.py
+++ python1.py
@@ -7,376 +7,187 @@ from nltk import word_tokenize
 from nltk import PorterStemmer
 
 
-from bs4 import BeautifulSoup
-
-
 class MyHTMLParser(HTMLParser, ABC):
     temp = "a"
     temp2 = "b"
-
     def handle_data(self, data):
         self.temp += data
         return self.temp
 
-index_query_id = 0
-query_id = []
-query_id.append(202)
-query_id.append(214)
-query_id.append(216)
-query_id.append(221)
-query_id.append(227)
-query_id.append(230)
-query_id.append(234)
-query_id.append(243)
-query_id.append(246)
-query_id.append(250)
+path = 'C:/Users/Hp/source/repos/PythonApplication1/PythonApplication1/corpus/*'
+files = glob.glob(path)
+index = 1         #index of term in document
+index_docid = 0   #document ids
+index_termid = 0
 
-debug = 0
-index = 0  # document ids
-index_termid = 1
+hash_map = {}
+key_hash_map = 0
+term_doc_list = []
 stemmedlist = []
 total_word_count = []
 dictionary = []
 doc_ids = []
 list_of_doc = []
 doc_index_term = []
-documents_data = []  # containing data for each document e.g data for document one is one first index
-documents_data_index = 1
+documents_data = []  #containing data for each document e.g data for document one is one first index
+documents_data_index = 0
+for name in files:
+    list_of_doc.clear()
+  #  print(name, '/', index)
+    docfile = open("docids.txt", "a+")
+    docids = docfile.write(name[70:99] + "\t" + str(index_docid) + "\n")
+    docfile.close()
 
-stemmedlist1 = []
-total_word_count1 = []
-dictionary1 = []
-doc_ids1 = []
-list_of_doc1 = []
-doc_index_term1 = []
+    File = open(name, errors='ignore')
+    contents = File.read()
 
-term_data = []
-# f = open("TERMID.txt", "r")
+    parser = MyHTMLParser()
+    parser.feed(contents)
 
-f = open("docids.txt", "r")
+    tokenized_aray = word_tokenize(parser.temp.lower())
+
+    thestoplist = open('stoplist.txt', 'r')
+    thestopwordaray = thestoplist.read()
+
+    after_stop_word = [word for word in tokenized_aray if word not in thestopwordaray]
+    #print(after_stop_word)
+    thestoplist.close()
+
+    ps = PorterStemmer()
+
+    after_stop_word = [''.join(c for c in s if c not in string.punctuation) for s in after_stop_word]
+    after_stop_word = [s for s in after_stop_word if s]
+    after_stop_word = [x for x in after_stop_word if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+    after_stop_word = [i for i in after_stop_word if len(i) > 1]
+
+    index = 1
+
+    for a in after_stop_word:
+        afterstem = ps.stem(a)
+        stemmedlist.append(afterstem)
+
+        if afterstem not in dictionary:
+            dictionary.append(afterstem)
+            index_termid = dictionary.index(afterstem)
+            termfile = open("termids.txt", "a+", errors='ignore')
+            termids = termfile.write(str(dictionary[index_termid]) + "\t" + str(index_termid) + "\n")
+            termfile.close()
+            hash_map[dictionary.index(afterstem)] = [(index_docid, index)]
+        else:
+            hash_map[dictionary.index(afterstem)].append((index_docid, index))
+
+        term_doc_list.append([dictionary.index(afterstem),index_docid,index])
+        index = index + 1
+
+        #print(term_doc_list[just_temp])
+        #just_temp = just_temp + 1
+
+    documents_data.append(stemmedlist[:])
+    stemmedlist.clear()
+
+
+
+
+    index_docid = index_docid + 1
+
+    #print(documents_data)
+
+File.close()
+
+#By sorting algorithm
+
+term_doc_list.sort(key=lambda x: x[0])
+
+just_temp = 0
+temp = []
+postings = []
+count_docs = []
+temp_count_docs = []
+count_corpus = []
+temp_count_corpus = 0
+delta = 0
+
+for this in term_doc_list:
+    #print(this)
+    if this[0] == just_temp + 1:
+        postings.append(temp[:])
+        temp.clear()
+        count_docs.append(len(temp_count_docs))
+        temp_count_docs.clear()
+        count_corpus.append(temp_count_corpus)
+        temp_count_corpus = 0
+        delta = 0
+
+
+    if this[1] not in temp_count_docs:
+        temp_count_docs.append(this[1])
+
+    temp_count_corpus = temp_count_corpus + 1
+
+    temp.append([this[1] - delta, this[2]])
+    just_temp = this[0]
+    delta = this[1]
+
+postings.append(temp[:])
+temp.clear()
+count_docs.append(len(temp_count_docs))
+temp_count_docs.clear()
+count_corpus.append(temp_count_corpus)
+temp_count_corpus = 0
+
+
+#for this in postings:
+#    print(postings.index(this))
+#    print(this)
+
+
+
+#for this in count_docs:
+    #print(count_docs.index(this))
+    #print(this)
+
+#for this in count_corpus:
+    #print(this)
+
+
+#for a in range(len(dictionary)):
+    #print(str(a) + "\t" + str(count_corpus[a]) + "\t" + str(count_docs[a]) + "\t" + str(postings[a]) + "\t" "\n")
+
+
+sorting = open("with_sorting.txt", "a+", errors='ignore')
+for a in range(len(dictionary)):
+    any = sorting.write(str(a) + "\t" + str(count_corpus[a]) + "\t" + str(count_docs[a]) + "\t" + str(postings[a]) + "\t" "\n")
+
+sorting.close()
+
+#hashmap
+#print(hash_map)
+
+
+h = open("with_hash_map.txt", "a+", errors='ignore')
+for a in range(len(dictionary)):
+    any1 = h.write(str(a) + "\t" + str(count_corpus[a]) + "\t" + str(count_docs[a]) + "\t" + str(hash_map[a]) + "\t" "\n")
+
+h.close()
+
+#reading
+read = 'http'
+term_data = 0
+found_id = 0
+f = open("termids.txt", "r")
+
 for a in f:
-    index = index + 1
+    term_data = a.split()
+    if read in term_data:
+        found_id = term_data[1]
+        print(found_id)
+
 f.close()
-thestoplist1 = open('stoplist.txt', 'r')
-stopwords = thestoplist1.read()
 
-path2 = 'C:/Users/Hp/source/repos/PythonApplication1/topics/*'
-queryFile1 = glob.glob(path2)
-for queryFile in queryFile1:
-    with open(queryFile) as fp:
-        querySoup = BeautifulSoup(fp, 'html.parser')
-
-        queries = querySoup.findAll("topic")
-        for query in queries:
-            #print('\n' + query['number'])
-            queryText = query.query.get_text().split()
-
-            # print((query.description.get_text('strip = True')).strip())
-
-            ##  stop and stemm query
-            queryText = [word for word in queryText if not word in stopwords]
-            pStemmer = PorterStemmer()
-            queryText = [pStemmer.stem(word) for word in queryText]
-            stemmedlist1.append(queryText)
-
-            queryfile = open("queries.txt", "a+")
-            docids = queryfile.write(str(queryText) + "\t" + "\n")
-            queryfile.close()
-
-docid = 0
-delta = 0
-average_doc_lengths = 0
-k1 = 1.2
-k2 = 10
-b = 0.75
-size_query = len(stemmedlist1)
-
-temp_array = []
-
-
-document_freq = []
-h = 0
-inverted = open("term_index.txt", "r")
+inverted = open("with_hash_map.txt", "r")
 for a in inverted:
     term_data = a.split()
-
-    document_freq.append(term_data[2])
-inverted.close()
-
-term_freq_in_corpus = []
-size_of_corpus = 0
-
-inverted = open("term_index.txt", "r")
-for a in inverted:
-    term_data = a.split()
-
-    term_freq_in_corpus.append(term_data[1])
-    size_of_corpus = size_of_corpus + int(term_data[1])
-
-#print('size of corpus : ' + str(size_of_corpus))
-#debug = input()
-
-inverted.close()
-
-bol = 0
-document_lengths = []
-dl = 0
-for count in range(index):
-    df = 0
-    inverted = open("term_index.txt", "r")
-    for a in inverted:
-        term_data = a.split()
-        for now in term_data:
-            if ',' in now:
-                docid = now.split(",")
-                # print(just[0])
-                if int(docid[0]) != 0 and bol == 1:
-                    bol = 0
-
-                if int(docid[0]) + delta == count + 1 or bol == 1:
-                    dl = dl + 1
-                    bol = 1
-                else:
-                    delta = delta + int(docid[0])
-    document_lengths.append(dl)
-    average_doc_lengths = average_doc_lengths + dl
-    inverted.close()
-delta = 0
-
-average_doc_lengths = average_doc_lengths / index
-
-
-bol = 0
-term_id = 0
-# first formula
-index_query_word = 0
-bm25 = []
-temp_bm25 = []
-
-for y in range(index):  # loop for document
-    for h in range(size_query):  # loop for query
-        index_query_word = 0
-
-        for z in stemmedlist1[h]:
-            index_query_word = index_query_word + 1
-            term_id = 0
-            f = open("TERMID.txt", "r")
-
-            for a in f:
-                term_data = a.split()
-                if z in term_data:
-                    term_id = term_data[0]
-                    #print('termid : ' + str(term_id))
-
-                    #print(z)
-                    #debug = input()
-            f.close()
-
-            #print('termid : ' + str(term_id))
-
-            # first value
-
-            df = 0
-            tf = 0
-            first_value = index + 0.5
-            #print('termid : ' + str(document_freq))
-            #term_id = input()
-            first_value = first_value / (int(document_freq[int(term_id) - 1]) + 0.5)
-            first_value = math.log(first_value)
-            #print('document freq : ' + document_freq[int(term_id) - 1])
-            #debug = input()
-
-            # second value
-            delta = 0
-            inverted = open("term_index.txt", "r")
-            for a in inverted:
-                term_data = a.split()
-                if term_id == term_data[0]:
-                    for now in term_data:
-                        if ',' in now:
-
-                            #print(' now in loop and docid : ' + docid[0])
-                            #debug = input()
-                            docid = now.split(",")
-                            #print(just[0])
-                            if int(docid[0]) != 0 and bol == 1:
-                                bol = 0
-
-                            if int(docid[0]) + delta == y or bol == 1:
-                                tf = tf + 1
-                                bol = 1
-                            else:
-                                delta = delta + int(docid[0])
-            #print('term freq : ' + str(tf) )
-            #debug = input()
-
-                # print(term_data[0] + '\t' + term_data[1])
-                # print(term_data)
-
-            inverted.close()
-
-
-            # find k
-            k = document_lengths[y] / average_doc_lengths
-            k = k * b
-            k = k + (1 - b)
-            k = k * k1
-
-            second_value = 1 + k1
-            second_value = second_value * tf
-            second_value = second_value / (k + tf)
-
-            # third value
-            third_value = 1 + k2
-            third_value = third_value / k2
-
-            if index_query_word == 1:
-                final_value = first_value * second_value * third_value
-
-            else:
-                final_value = final_value*first_value * second_value * third_value
-
-        #print(final_value)
-
-        temp_bm25.append([query_id[index_query_id], y, final_value])
-        index_query_id = index_query_id + 1
-
-    index_query_id = 0
-
-temp_bm25.sort(key=lambda x: x[0])
-
-ranking = []
-
-#print(temp_bm25)
-#debug = input()
-
-for a in query_id:
-    #print(a)
-    #debug = input()
-    for b in temp_bm25:
-        if a == b[0]:
-            ranking.append(b)
-    ranking.sort(key=lambda x: x[2])
-    for b in ranking:
-        bm25.append(b)
-    ranking.clear()
-
-rank = index
-scores = open("score1.txt", "a+")
-
-for a in bm25:
-    docids = scores.write(str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\t " + str(rank) + "\t " + "\n")  # query number / doc number / score
-    rank = rank - 1
-    if rank == 0:
-        rank = index
-
-scores.close()
-
-
-
-index_query_id = 0
-#method 2
-
-dirichlet = []
-temp_dirichlet = []
-final_value2 = 0
-big_box = 0
-small_box = 0
-for y in range(index):  #loop for document
-    for h in range(size_query):
-        index_query_word = 0
-        for z in stemmedlist1[h]:
-            index_query_word = index_query_word + 1
-            term_id = 0
-            f = open("TERMID.txt", "r")
-
-            for a in f:
-                term_data = a.split()
-                if z in term_data:
-                    term_id = term_data[0]
-                    # print('termid : ' + str(term_id))
-
-                    # print(z)
-                    # debug = input()
-            f.close()
-
-            # print('termid : ' + str(term_id))
-
-            # first value
-            delta = 0
-            inverted = open("term_index.txt", "r")
-            for a in inverted:
-                term_data = a.split()
-                if term_id == term_data[0]:
-                    for now in term_data:
-                        if ',' in now:
-                            docid = now.split(",")
-                            #print(just[0])
-                            if int(docid[0]) != 0 and bol == 1:
-                                bol = 0
-
-                            if int(docid[0]) + delta == y or bol == 1:
-                                tf = tf + 1
-                                bol = 1
-                            else:
-                                delta = delta + int(docid[0])
-
-            small_box = tf / document_lengths[y]
-
-            first_value = document_lengths[y]
-            first_value = first_value / (document_lengths[y] + 5)
-            first_value = first_value * small_box
-
-
-            #second value
-
-            big_box = int(term_freq_in_corpus[int(term_id)]) / int(size_of_corpus)
-
-            second_value = 5
-            second_value = second_value / (document_lengths[y] + 5)
-            second_value = second_value * big_box
-
-
-            if index_query_word == 1:
-                final_value2 = first_value + second_value
-
-            else:
-                final_value2 = final_value2 * (first_value + second_value)
-
-            #print(final_value2)
-
-        temp_dirichlet.append([query_id[index_query_id], y, final_value2])
-        index_query_id = index_query_id + 1
-
-    index_query_id = 0
-
-temp_dirichlet.sort(key=lambda x: x[0])
-
-ranking2 = []
-
-#print(temp_dirichlet)
-#debug = input()
-
-
-for a in query_id:
-    for b in temp_dirichlet:
-        if a == b[0]:
-            ranking2.append(b)
-    ranking2.sort(key=lambda x: x[2])
-    for b in ranking2:
-        dirichlet.append(b)
-    ranking2.clear()
-
-
-rank = index
-scores2 = open("score2.txt", "a+")
-
-for a in dirichlet:
-    docids = scores2.write(str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\t " + str(rank) + "\t " + "\n")  # query number / doc number / score
-    rank = rank - 1
-    if rank == 0:
-        rank = index
-
-scores2.close()
+    if found_id == term_data[0]:
+        print(found_id , term_data[1] , term_data[2])
 
 
